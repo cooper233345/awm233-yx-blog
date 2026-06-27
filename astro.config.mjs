ce267ff8a4dde797e714d4a0bed5579dc9b4eca4
cooper233345 <2070745405@qq.com>
2026-06-28 02:48:50 +0800

    Update site URL to CF Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://awm233-yx.vercel.app',
+	site: 'https://awm233-yx-blog.pages.dev',
 	integrations: [mdx(), sitemap()],
 });
